4beea4c51a7947db4f92e66d6194f2926f291e7c
darkpink <wmikeking@gmail.com>
2015-04-19 14:29:12 -0500

    initial work on object messages

---
+++ CommonComponents/Messages/ObjectMessage.cs
@@ -0,0 +1,37 @@
+﻿using Informagator.Contracts;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Informagator.CommonComponents.Messages
+{
+    public class ObjectMessage<TBody> : MessageBase<TBody>
+    {
+        protected BinaryFormatter Formatter { get; set; }
+
+        public override byte[] BinaryData
+        {
+            get
+            {
+                byte[] result;
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    Formatter.Serialize(stream, Body);
+                }
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public ObjectMessage()
+        {
+            Formatter = new BinaryFormatter();
+        }
+    }
+}
---
+++ CommonComponents/SupplierStages/DatabaseObjectSupplier.cs
@@ -0,0 +1,79 @@
+﻿using Informagator.Contracts.Attributes;
+using Informagator.Contracts.Exceptions;
+using Informagator.Contracts.Stages;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Informagator.CommonComponents.SupplierStages
+{
+    public abstract class DatabaseObjectSupplier<TObject> : ISupplierStage
+        where TObject : new()
+    {
+        [ConfigurationParameter]
+        public string Server { get; set; }
+
+        [ConfigurationParameter]
+        public string Database { get; set; }
+        
+        protected abstract string SqlStatement { get; }
+
+        protected SqlConnection Connection { get; set; }
+
+        public Contracts.IMessage Supply()
+        {
+            TObject
+            if (Connection == null)
+            {
+                RebuildConnection();
+            }
+
+            if (Connection != null)
+            {
+                using (SqlCommand cmd = new SqlCommand(SqlStatement, Connection))
+                {
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                }
+            }
+        }
+
+        private void RebuildConnection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsBlocking
+        {
+            get { return false; }
+        }
+
+        public string ReceviedFrom
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string Name
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void ValidateSettings()
+        {
+            if (String.IsNullOrWhiteSpace(Server))
+            {
+                throw new ConfigurationException("Server is a required parameter for " + Name + " [" + this.GetType() + "]");
+            }
+
+            if (String.IsNullOrWhiteSpace(Database))
+            {
+                throw new ConfigurationException("Database is a required parameter for " + Name + " [" + this.GetType() + "]");
+            }
+
+
+        }
+    }
+}
